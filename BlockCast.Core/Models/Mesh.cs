e29bc3711279e8f3e69529b9e07cc2f447a373fb
AlexHysel <alexhysel@gmail.com>
2026-05-24 14:06:05 +0800

    Add 2 constructors for Triangle

--- BlockCast.Core/Models/Mesh.cs
+++ BlockCast.Core/Models/Mesh.cs
@@ -8,4 +8,23 @@ public class Mesh
     public List<Triangle> Normals { get; set; } = [];
 }
 
-public record Triangle(int A, int B, int C);
+public record Triangle
+{
+    public int A {get; init;}
+    public int B {get; init;}
+    public int C {get; init;}
+
+    public Triangle(int a, int b, int c)
+    {
+        A = a;
+        B = b;
+        C = c;
+    }
+
+    public Triangle(int[] vertices)
+    {
+        A = vertices[0];
+        B = vertices[1];
+        C = vertices[2];
+    }
+}
